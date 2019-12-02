49bff3307e6d33f4bc9101cd08c819c0467c941a
valvv <33302737+ValWV@users.noreply.github.com>
2019-12-03 02:27:16 +0300

    f

--- components/bitrix/news.list/template_news_cards/news.js
+++ components/bitrix/news.list/template_news_cards/news.js
@@ -11,3 +11,16 @@ var swiper = new Swiper('.blog-slider', {
       clickable: true,
     }
   });
+
+ // var news = '<div class="blog-slider__item swiper-slide">'+
+ // '<div class="blog-slider__img">'+
+    
+ //   '<img src="'++'" alt="">'+
+ // '</div>'+
+ // '<div class="blog-slider__content">'+
+  //  '<span class="blog-slider__code">'+ +'</span>'+
+ //   '<div class="blog-slider__title">'+ +'</div>'+
+ //   '<div class="blog-slider__text">'+ +'</div>'+
+ //   '<a href="#" class="blog-slider__button">'+ +'</a>'+
+ // '</div>'+
+//'</div>';
